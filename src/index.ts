e0259b90659dd04deafba23b9e72e2ab3512812d
philippe.sarafian <philippe.sarafian@groupecimes.com>
2022-06-30 15:46:38 +0200

    Npm packaging (need to see how to structure final npm package)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-export {default as Client} from './CustomClient';
+export {TableDataID, default as CustomClient} from './CustomClient';
+export {CommandInfo, default as Command} from './interactions/Command';
