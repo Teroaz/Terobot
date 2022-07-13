683297ee6562dbc284a2babbf320b2d9bb3615d0
philippe.sarafian <philippe.sarafian@groupecimes.com>
2022-07-13 16:23:17 +0200

    Loaders + Enhancements

--- src/CustomClient.ts
+++ src/CustomClient.ts
@@ -1,25 +1,26 @@
 import {Client, Collection, Guild, GuildMember, Intents, Permissions, Snowflake} from 'discord.js';
 import * as path from 'path';
 import Command from './interactions/Command';
+import {Loaders} from './Loaders';
 
 export type TableDataID = Record<string, Snowflake>;
 
 export default abstract class CustomClient extends Client {
-	
-	readonly chansID: TableDataID = require(path.join(__dirname, 'datas/chansID.json'));
-	readonly rolesID: TableDataID = require(path.join(__dirname, 'datas/rolesID.json'));
-	
+
+	readonly chansID: TableDataID = require(path.join(process.cwd(), 'datas/chansID.json'));
+	readonly rolesID: TableDataID = require(path.join(process.cwd(), 'datas/rolesID.json'));
+
 	staff: Record<'rolesID' | 'usersID', Array<Snowflake>> = {
 		rolesID: [],
 		usersID: []
 	};
-	
+
 	readonly commands: Collection<string, Command> = new Collection();
-	
+
 	get baseGuild(): Guild | null {
 		return this.guilds.cache.get(process.env.BASE_GUILD_ID!) || null;
 	}
-	
+
 	protected constructor() {
 		super({
 			intents: [
@@ -43,18 +44,24 @@ export default abstract class CustomClient extends Client {
 			allowedMentions: {parse: ['roles', 'users', 'everyone'], repliedUser: false},
 			partials: ['MESSAGE', 'GUILD_SCHEDULED_EVENT', 'GUILD_MEMBER', 'CHANNEL', 'REACTION', 'USER']
 		});
-		
+
 		this.login(process.env.TOKEN).then(() => {
+			Loaders.loadCommands(this);
+			Loaders.loadEvents(this);
+			Loaders.loadStaff(this);
 			this.onLogin();
+		}).catch(e => {
+			console.error(`Cannot instantiate CustomClient ${e}`);
+			process.exit(1);
 		});
 	}
-	
+
 	isStaff = (member: GuildMember) => member.permissions.has(Permissions.FLAGS.BAN_MEMBERS) && member.roles.cache.some(r => this.staff.rolesID.includes(r.id)) || this.staff.usersID.includes(member.id);
-	
+
 	addStaff = (type: 'rolesID' | 'usersID', id: Snowflake) => {
 		if (this.staff[type].includes(id)) return;
 		this.staff[type].push(id);
 	}
-	
-	abstract onLogin();
+
+	protected abstract onLogin();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
 export * from './exceptions';
 export * from './interactions';
-export * from './utils';
 
+export * from './utils';
 export {default as CustomClient} from './CustomClient';
+
 export {Loaders} from './Loaders';
+
+const envFiles = fs.readdirSync(process.cwd()).filter(file => file.startsWith('.env') && !file.includes('example'));
+
+if (envFiles.length === 0) {
+	throw new Error('No .env file found');
+}
+
+if (envFiles.length === 1) {
+	dotenv.config({path: `${process.cwd()}/${envFiles[0]}`});
+} else {
+	if (!process.env.NODE_ENV) {
+		throw new Error('No NODE_ENV found');
+	}
+	dotenv.config({path: `${process.cwd()}/.env${process.env.NODE_ENV === 'production' ? '.prod' : '.dev'}`});
+}
+
+process.on('uncaughtException', (e: Error) => {
+	console.warn(`Captured uncaught exception ${e.stack}`);
+});
+
+process.on('unhandledRejection', (e: Error) => {
+	console.warn(`Captured unhandled rejection : ${e.stack}`);
+});
